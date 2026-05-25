4d8ad8a02aeee194eed3aed437bab1b93171ecae
Sofia <soniakats11@gmail.com>
2026-05-25 15:33:35 +0300

    new commit

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,57 +1,62 @@
-import { useState } from 'react'
-import './App.css'
-import MyList from './MyList'
-import MyMealsAndIngredients from './MyMealsAndIngredients'
-
+import { useState, useEffect } from 'react';
+import { v4 as uuid } from 'uuid';  
+import './App.css';
+import MyList from './MyList';
+import MyMealsAndIngredients from './MyMealsAndIngredients';
 
 function App() {
+    const [mealPlans, setMealPlans] = useState(
+        localStorage.mealPlans ? JSON.parse(localStorage.mealPlans) : []
+    );
+    const [selectedDay, setSelectedDay] = useState(false);
 
-  const [mealPlans , setMealPlans] = useState(localStorage.mealPlans ? JSON.parse(localStorage.mealPlans):[])
-  const [selectedDay , setSelectedDay] = useState(false)
-  const addMeal = () => {
-    const newMeal = {
-      title:'Today is ...',
-      id:uuid(),
-      mealForADay:'',ingredients:''
-    }
+    const addMeal = () => {
+        const newMeal = {
+            title: 'Today is ...',
+            id: uuid(),  
+            mealForADay: '',
+            ingredients: ''
+        };
+        setMealPlans([newMeal, ...mealPlans]); 
+    };
 
-  }
-  
-  
-  setMealPlans([newMeal,...mealPlans])
+    const deleteDay = (mealId) => {
+        setMealPlans(mealPlans.filter(({ id }) => id !== mealId));
+    };
 
-  const deleteDay = (mealId) => {
-    setMealPlans(mealPlans.filter(({id}) => id !== mealId))
-  }
+    const updateDay = (updatedMeal) => { 
+        const updatedMeals = mealPlans.map((mealPlan) => {
+            if (mealPlan.id === updatedMeal.id) {  
+                return updatedMeal;
+            }
+            return mealPlan;
+        });
+        setMealPlans(updatedMeals);
+    };
 
-  const updateDay = (mealUpdateMeal) => {
-    const updateMeals = mealPlans.map((mealPlan) => {
-      if (mealPlan.id === myUpdateMeal.id){
-        return myUpdateMeal;
-      }
-      return mealPlan;
-    })
-    setMealPlans(updateMeals)
-  }
+    const ActivateMeal = () => {
+        return mealPlans.find(({ id }) => id === selectedDay);
+    };
 
-  
+    useEffect(() => {
+        localStorage.setItem('mealPlans', JSON.stringify(mealPlans));
+    }, [mealPlans]);
 
-  const ActivateMeal = () => {
-    return mealPlans.find(({id}) => id === selectedDay)
-  }
-
-  return (
-    <div className='App'>
-      <MyList
-        mealPlans={mealPlans}
-        addMeal={addMeal}
-        deleteDay={deleteDay}
-        selectedDay={selectedDay}
-        setSelectedDay={setSelectedDay}
-      />
-      <MyMealsAndIngredients selectedDay={ActivateMeal()} updateDay={updateDay}/>
-    </div>
-  )
+    return (
+        <div className='App'>
+            <MyList
+                mealPlans={mealPlans}
+                addMeal={addMeal}
+                deleteDay={deleteDay}
+                selectedDay={selectedDay}
+                setSelectedDay={setSelectedDay}
+            />
+            <MyMealsAndIngredients 
+                selectedDay={ActivateMeal()} 
+                updateDay={updateDay}
+            />
+        </div>
+    );
 }
 
-export default App
+export default App;
